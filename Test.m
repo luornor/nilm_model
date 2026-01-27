aa02e483bfc10d3dd020683210d8d3e490ed65a9
luornor <tetteynathan89@gmail.com>
2026-01-27 23:20:08 +0000

    deleted:    Project Documentation v2.docx 	deleted:    Project Documentation.docx 	deleted:    Test.m 	renamed:    export_natural_data_to_csv.m -> matlab_to_csv/export_natural_data_to_csv.m 	renamed:    export_simulated_data_to_csv.m -> matlab_to_csv/export_simulated_data_to_csv.m 	renamed:    export_synthetic_data_to_csv.m -> matlab_to_csv/export_synthetic_data_to_csv.m

--- Test.m
+++
@@ -1,8 +0,0 @@
-% 1) Do we have channel 2 anywhere?
-d2 = dir(fullfile(pwd,'**','Samples_*.2.bin'));
-d2u = dir(fullfile(pwd,'**','Samples_*_2.bin'));
-disp(length(d2)); disp(length(d2u));
-
-% 2) Show one sample's bin filenames
-d = dir(fullfile(pwd,'Natural','**','Samples_003*.bin'));
-{d.name}'
